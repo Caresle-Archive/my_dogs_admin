2dfa7059feae4fd4f6361d33b30a9f4730b062b0
caresle <carabesle@gmail.com>
2023-10-11 21:36:13 -0600

    Add dog factory

--- app/Models/DogType.php
+++ app/Models/DogType.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class DogType extends Model
 {
@@ -19,4 +20,8 @@ class DogType extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function dog(): BelongsToMany {
+        return $this->belongsToMany(Dog::class, 'id_dog_type', 'id');
+    }
 }
---
+++ database/factories/DogFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dog;
+use App\Models\DogType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dog>
+ */
+class DogFactory extends Factory
+{
+    protected $model = Dog::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->firstName(),
+            'height' => fake()->randomFloat(2, 1, 1000),
+            'weight' => fake()->randomFloat(2, 1, 1000),
+            'id_dog_type' => function () {
+                return DogType::factory()->create()->id;
+            }
+        ];
+    }
+}
