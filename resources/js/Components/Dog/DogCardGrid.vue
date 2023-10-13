14cbc2462cb9e2d017a414d35ec94f70a4e602df
caresle <carabesle@gmail.com>
2023-10-12 20:37:04 -0600

    Add actions buttons en dog card grid

--- resources/js/Components/Dog/DogCardGrid.vue
+++ resources/js/Components/Dog/DogCardGrid.vue
@@ -13,19 +13,48 @@ import {
     mdiPencil,
     mdiTrashCan,
 } from '@mdi/js';
+import { router } from '@inertiajs/vue3';
+import { ref } from 'vue';
+import ModalNormal from '@/Components/Modal/ModalNormal.vue';
 
 defineProps({
     dogs: Array,
 });
+
+const goToEdit = (id) => {
+    router.visit(route('dogs.edit', id));
+}
+
+const openModal = (id) => {
+    dogToDelete.value = id;
+    showModal.value = true;
+};
+
+const closeModal = () => {
+    showModal.value = false;
+};
+
+const handleOk = () => {
+    router.delete(route('dogs.destroy', dogToDelete.value), {
+        onFinish: () => showModal.value = false,
+    });
+};
+
+const dogToDelete = ref(0);
+const showModal = ref(false);
 </script>
 
 <template>
     <div class="max-w-5xl mx-auto grid grid-cols-3 p-2">
+        <ModalNormal @cancel="closeModal" title="Delete Dog"
+            text="Are you sure to delete the dog?" :show="showModal"
+            @ok="handleOk"
+        />
         <TheCard v-for="(dog, ind) in dogs" :key="ind"
             class="mx-2 my-3"
         >
-            <div class="flex items-center">
-                <Heading tag="h3" class="mb-4">{{ dog.name }}</Heading>
+            <div class="flex items-center mb-2">
+                <Heading tag="h3">{{ dog.name }}</Heading>
                 <Dropdown text="Bottom">
                     <template #trigger>
                         <svgIcon :path="mdiDotsHorizontal" type="mdi"
@@ -34,11 +63,15 @@ defineProps({
                     </template>
                     <template #default>
                         <ListGroup>
-                            <ListGroupItem class="text-gray-400">
+                            <ListGroupItem class="text-gray-400"
+                                @click="() => goToEdit(dog.id)"
+                            >
                                 <svgIcon :path="mdiPencil" type="mdi" />
                                 <span class="ml-4">Edit</span>
                             </ListGroupItem>
-                            <ListGroupItem class="text-red-500">
+                            <ListGroupItem class="text-red-500"
+                                @click="() => openModal(dog.id)"
+                            >
                                 <svgIcon :path="mdiTrashCan" type="mdi" />
                                 <span class="ml-4">Delete</span>
                             </ListGroupItem>
@@ -47,12 +80,20 @@ defineProps({
                 </Dropdown>
             </div>
             <div>
+                <div class="w-32 mx-auto mb-5">
+                    <img src="https://placehold.co/1200x1200/fc03ba/FFFFFF/png" alt=""
+                        class="rounded-full"
+                    />
+                </div>
                 <div>
                     <P>
-                        Height: {{ dog.height }}
+                        <strong>Type:</strong> {{ dog.dog_type.name }}
                     </P>
                     <P>
-                        Weight: {{ dog.weight }}
+                        <strong>Height:</strong> {{ dog.height }}
+                    </P>
+                    <P>
+                        <strong>Weight:</strong> {{ dog.weight }}
                     </P>
                 </div>
             </div>
