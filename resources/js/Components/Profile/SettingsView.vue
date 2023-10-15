2b16b0e222a63fec5a758bb1664c68e5854b0ad3
caresle <carabesle@gmail.com>
2023-10-14 21:14:27 -0600

    Start with settings view

--- resources/js/Components/Profile/SettingsView.vue
+++ resources/js/Components/Profile/SettingsView.vue
@@ -1,9 +1,32 @@
 <script setup>
 import {
     Heading,
+    TheCard,
+    Input,
+    Button,
 } from 'flowbite-vue';
 </script>
 
 <template>
     <Heading tag="h2">Settings</Heading>
+
+    <TheCard variant="image" class="!max-w-full mt-4">
+        <Heading tag="h4">Profile</Heading>
+        <div>
+            <form>
+                <Input label="Username" placeholder="Username" />
+                <div class="flex w-full">
+                    <Input class="w-full mr-1" label="Password" placeholder="Password"
+                        type="password"
+                    />
+                    <Input class="w-full ml-1" label="Confrim Password"
+                        placeholder="Confirm Password" type="password"
+                    />
+                </div>
+                <div class="flex justify-center">
+                    <Button class="mt-2 w-1/3">Update</Button>
+                </div>
+            </form>
+        </div>
+    </TheCard>
 </template>
