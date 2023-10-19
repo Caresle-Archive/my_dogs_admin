65ae5e16bfd650a9045a677f0bbc9edd9903029c
caresle <carabesle@gmail.com>
2023-10-18 21:35:28 -0600

    Add middleware has right to rol routes

--- routes/web/rol.php
+++ routes/web/rol.php
@@ -2,5 +2,35 @@
 
 use App\Http\Controllers\RolController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Helpers\PermissionHelper;
 
-Route::resource('/rol', RolController::class);
+Route::get("/rol", [RolController::class, "index"])
+    ->name("rol.index")
+    ->middleware("has_right:" . PermissionHelper::GET_ROLE);
+
+Route::post("/rol", [RolController::class, "store"])
+    ->name("rol.store")
+    ->middleware("has_right:" . PermissionHelper::POST_ROLE);
+
+Route::get("/rol/create", [RolController::class, "create"])
+    ->name("rol.create")
+    ->middleware("has_right:" . PermissionHelper::POST_ROLE);
+
+Route::get("/rol/{rol}", [RolController::class, "show"])
+    ->name("rol.show")
+    ->middleware("has_right:" . PermissionHelper::GET_ROLE);
+
+Route::put("/rol/{rol}", [RolController::class, "update"])
+    ->name("rol.update")
+    ->middleware("has_right:" . PermissionHelper::PUT_ROLE);
+
+Route::delete("/rol/{rol}", [RolController::class, "destroy"])
+    ->name("rol.destroy")
+    ->middleware("has_right:" . PermissionHelper::DELETE_ROLE);
+
+Route::get("/rol/{rol}/edit", [RolController::class, "edit"])
+    ->name("rol.edit")
+    ->middleware("has_right:" . PermissionHelper::PUT_ROLE);
+
+
+
--- routes/web/user.php
+++ routes/web/user.php
@@ -4,35 +4,30 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Helpers\PermissionHelper;
 
-$get_user = PermissionHelper::GET_USER;
-$post_user = PermissionHelper::POST_USER;
-$put_user = PermissionHelper::PUT_USER;
-$delete_user = PermissionHelper::DELETE_USER;
-
 Route::get('/users', [UserController::class, 'index'])
     ->name('users.index')
-    ->middleware("has_right:$get_user");
+    ->middleware("has_right:" . PermissionHelper::GET_USER);
 
 Route::post('/users', [UserController::class, 'store'])
     ->name('users.store')
-    ->middleware("has_right:$post_user");
+    ->middleware("has_right:" . PermissionHelper::POST_USER);
 
 Route::get('/users/create', [UserController::class, 'create'])
     ->name('users.create')
-    ->middleware("has_right:$post_user");
+    ->middleware("has_right:" . PermissionHelper::POST_USER);
 
 Route::get('/users/{user}', [UserController::class, 'show'])
     ->name('users.show')
-    ->middleware("has_right:$get_user");
+    ->middleware("has_right:" . PermissionHelper::GET_USER);
 
 Route::put('/users/{user}', [UserController::class, 'update'])
     ->name('users.update')
-    ->middleware("has_right:$put_user");
+    ->middleware("has_right:" . PermissionHelper::PUT_USER);
 
 Route::delete('/users/{user}', [UserController::class, 'destroy'])
     ->name('users.destroy')
-    ->middleware("has_right:$delete_user");
+    ->middleware("has_right:" . PermissionHelper::DELETE_USER);
 
 Route::get('/users/{user}/edit', [UserController::class, 'edit'])
     ->name('users.edit')
-    ->middleware("has_right:$put_user");
+    ->middleware("has_right:" . PermissionHelper::PUT_USER);
