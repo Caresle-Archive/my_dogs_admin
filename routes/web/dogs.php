644946ab586ec87956ef53de3a43816d321f6327
caresle <carabesle@gmail.com>
2023-10-18 21:40:09 -0600

    Add dogs and dogs type middleware

--- routes/web/dogs.php
+++ routes/web/dogs.php
@@ -3,4 +3,32 @@
 use App\Http\Controllers\DogsController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/dogs', DogsController::class);
+use App\Http\Helpers\PermissionHelper;
+
+Route::get("/dogs", [DogsController::class, "index"])
+    ->name("dogs.index")
+    ->middleware("has_right:" . PermissionHelper::GET_DOG);
+
+Route::post("/dogs", [DogsController::class, "store"])
+    ->name("dogs.store")
+    ->middleware("has_right:" . PermissionHelper::POST_DOG);
+
+Route::get("/dogs/create", [DogsController::class, "create"])
+    ->name("dogs.create")
+    ->middleware("has_right:" . PermissionHelper::POST_DOG);
+
+Route::get("/dogs/{dog}", [DogsController::class, "show"])
+    ->name("dogs.show")
+    ->middleware("has_right:" . PermissionHelper::GET_DOG);
+
+Route::put("/dogs/{dog}", [DogsController::class, "update"])
+    ->name("dogs.update")
+    ->middleware("has_right:" . PermissionHelper::PUT_DOG);
+
+Route::delete("/dogs/{dog}", [DogsController::class, "destroy"])
+    ->name("dogs.destroy")
+    ->middleware("has_right:" . PermissionHelper::DELETE_DOG);
+
+Route::get("/dogs/{dog}/edit", [DogsController::class, "edit"])
+    ->name("dogs.edit")
+    ->middleware("has_right:" . PermissionHelper::PUT_DOG);
--- routes/web/dogs_type.php
+++ routes/web/dogs_type.php
@@ -2,5 +2,32 @@
 
 use App\Http\Controllers\DogsTypeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Helpers\PermissionHelper;
 
-Route::resource('/dogs_type', DogsTypeController::class)->except('show');
+Route::get("/dogs_type", [DogsTypeController::class, "index"])
+    ->name("dogs_type.index")
+    ->middleware("has_right:" . PermissionHelper::GET_DOG_TYPE);
+
+Route::post("/dogs_type", [DogsTypeController::class, "store"])
+    ->name("dogs_type.store")
+    ->middleware("has_right:" . PermissionHelper::POST_DOG_TYPE);
+
+Route::get("/dogs_type/create", [DogsTypeController::class, "create"])
+    ->name("dogs_type.create")
+    ->middleware("has_right:" . PermissionHelper::POST_DOG_TYPE);
+
+Route::get("/dogs_type/{dogs_type}", [DogsTypeController::class, "show"])
+    ->name("dogs_type.show")
+    ->middleware("has_right:" . PermissionHelper::GET_DOG_TYPE);
+
+Route::put("/dogs_type/{dogs_type}", [DogsTypeController::class, "update"])
+    ->name("dogs_type.update")
+    ->middleware("has_right:" . PermissionHelper::PUT_DOG_TYPE);
+
+Route::delete("/dogs_type/{dogs_type}", [DogsTypeController::class, "destroy"])
+    ->name("dogs_type.destroy")
+    ->middleware("has_right:" . PermissionHelper::DELETE_DOG_TYPE);
+
+Route::get("/dogs_type/{dogs_type}/edit", [DogsTypeController::class, "edit"])
+    ->name("dogs_type.edit")
+    ->middleware("has_right:" . PermissionHelper::PUT_DOG_TYPE);
